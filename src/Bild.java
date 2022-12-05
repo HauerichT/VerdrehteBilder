d11c6c3addbb204057c44b341c4664896aea8928
timohaverich <timo.haverich@fh-bielefeld.de>
2022-12-05 18:59:04 +0100

    Akt

--- src/Bild.java
+++ src/Bild.java
@@ -1,11 +1,8 @@
-import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.List;
 
 public class Bild {
 
@@ -17,22 +14,34 @@ public class Bild {
 
     public void leseBild(String dateiName) throws IOException {
 
-        Path path = Paths.get("/Users/timohaverich/IdeaProjects/VerdrehteBilder/src/"+dateiName);
+        Path path = Paths.get("src/"+dateiName);
 
         if (Files.exists(path)) {
-            List<String> lines = Files.readAllLines(path);
-            System.out.println(lines);
+            String dataOutFile = Files.readString(path);
+            String[] dataInArray = dataOutFile.split("\r\n");
+
+            this.format = dataInArray[0].trim();
+            this.maxHelligkeit = Integer.parseInt(dataInArray[2].trim());
+            String[] formatArr = dataInArray[1].split(" ");
+            this.breite = Integer.parseInt(formatArr[0]);
+            this.hoehe = Integer.parseInt(formatArr[1].trim());
+
+            String[] temp;
+            int[][] bilddatenTemp = new int[hoehe][breite];
+            for (int i = 0; i < this.hoehe; i++) {
+                temp = dataInArray[i+3].split("\\s+");
+                for (int j = 0; j < this.breite; j++) {
+                    bilddatenTemp[i][j] = Integer.parseInt(temp[j]);
+                }
+            }
+            bilddaten = bilddatenTemp;
+            System.out.println("Format: " + this.format);
+            System.out.println("Breite: " + this.breite);
+            System.out.println("Höhe: " + this.hoehe);
+            System.out.println("Maximalwert für die Helligkeit: " + this.maxHelligkeit);
+            System.out.println("Bilddaten in einem 2D Array: "+Arrays.deepToString(this.bilddaten));
 
 
-            String size = lines.get(1);
-            String[] formatSplit = size.split(" ");
-            this.breite = Integer.parseInt(formatSplit[0]);
-            this.hoehe = Integer.parseInt(formatSplit[1]);
-            this.format = this.breite + " x " +  this.hoehe;
-
-            System.out.println(breite);
-            System.out.println(hoehe);
-            System.out.println(format);
 
         }
         else {
