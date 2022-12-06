6fcd40a229c350b4745e7037e5858e96aa3cbbf7
timohaverich <timo.haverich@fh-bielefeld.de>
2022-12-06 11:35:48 +0100

    Finished

--- src/Bild.java
+++ src/Bild.java
@@ -3,10 +3,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
-import java.util.Arrays;
 
 public class Bild {
 
+    // Instanzvariablen
     private int[][] bilddaten;
     private int[][] gedrehteBilddaten;
     private String format;
@@ -14,81 +14,155 @@ public class Bild {
     private int hoehe;
     private int maxHelligkeit;
 
+
+    // Methode liest das übergebene PGM Bild ein
     public void leseBild(String dateiName) {
 
-        Path path;
-        String data = "";
+        // Variablendeklaration
+        Path pfad;
+        String daten = "";
 
+        // versucht die übergebene Datei zu lesen
         try {
-            path = Paths.get("src/"+dateiName);
-            data = Files.readString(path);
+            pfad = Paths.get("src/"+dateiName);
+            // speichert die Daten des Bildes
+            daten = Files.readString(pfad);
         } catch (IOException e) {
             System.out.println("Die übergebene Datei existiert nicht!");
         }
 
-        String[] dataInArray = data.split("\r\n");
+        // schreibt die Daten des Bildes, getrennt am Zeilenumbruch, in ein String Array
+        String[] datenArray = daten.split("\r\n");
 
-        this.format = dataInArray[0].trim();
-        this.maxHelligkeit = Integer.parseInt(dataInArray[2].trim());
-        String[] formatArr = dataInArray[1].split(" ");
-        this.breite = Integer.parseInt(formatArr[0]);
-        this.hoehe = Integer.parseInt(formatArr[1].trim());
+        // entfernt überflüssige Leerzeichen und speichert das Format
+        this.format = datenArray[0].trim();
 
-        String[] temp;
-        int[][] bilddatenTemp = new int[hoehe][breite];
+        // entfernt überflüssige Leerzeichen und speichert die maximale Helligkeit
+        this.maxHelligkeit = Integer.parseInt(datenArray[2].trim());
+
+        // schreibt die Größe des Bildes in ein weiteres Array und speichert Höhe und Breite
+        String[] formatArray = datenArray[1].split(" ");
+        this.breite = Integer.parseInt(formatArray[0]);
+        this.hoehe = Integer.parseInt(formatArray[1].trim());
+
+        // speichert die aktuelle Zeile
+        String[] tempZeile;
+        // neues temporäres Array um Daten in Instanzvariable schreiben zu können
+        int[][] tempBilddaten = new int[this.hoehe][this.breite];
+
         for (int i = 0; i < this.hoehe; i++) {
-            temp = dataInArray[i+3].split("\\s+");
+            // trennt die Werte der Zeile an Leerzeichen
+            tempZeile = datenArray[i+3].split("\\s+");
             for (int j = 0; j < this.breite; j++) {
-                bilddatenTemp[i][j] = Integer.parseInt(temp[j]);
+                // prüft, ob Wert eine Zahl ist und schreibt diese in das temporäre Array
+                if (tempZeile[j].matches("\\d*")) {
+                    tempBilddaten[i][j] = Integer.parseInt(tempZeile[j]);
+                }
             }
         }
-        bilddaten = bilddatenTemp;
-
-        System.out.println("Format: " + this.format);
-        System.out.println("Breite: " + this.breite);
-        System.out.println("Höhe: " + this.hoehe);
-        System.out.println("Maximalwert für die Helligkeit: " + this.maxHelligkeit);
-        System.out.println("Bilddaten in einem 2D Array: " + Arrays.deepToString(this.bilddaten));
+        // schreibt das befüllte temporäre Array in bilddaten
+        this.bilddaten = tempBilddaten;
 
     }
 
+
+    // Methode dreht das verarbeitete Bild um 90 Grad gegen den Uhrzeigersinn
     public void rotateBild() {
-        int[][] gedrehteBilddatenTemp = new int[breite][hoehe];
+        // speichert die gedrehten Bilddaten temporär
+        int[][] tempGedrehteBilddaten = new int[breite][hoehe];
+        // dreht die Bilddaten und schreibt diese in das temporäre Array
         for (int i = 0; i < this.hoehe; i++) {
             for (int j = 0; j < this.breite; j++) {
-                gedrehteBilddatenTemp[j][i] = bilddaten[i][breite-j-1];
+                tempGedrehteBilddaten[j][i] = bilddaten[i][breite-j-1];
             }
         }
-        gedrehteBilddaten = gedrehteBilddatenTemp;
+        // schreibt die gedrehten temporären Bilddaten in gedrehteBilddaten
+        gedrehteBilddaten = tempGedrehteBilddaten;
     }
 
 
+    // Methode schreibt das gedrehte Bild Array in einen String und anschließend in eine neue PGM Datei
     public void schreibeBild(String verzeichnis){
 
+        // gibt das aktuelle Datum zurück
         LocalDate date = LocalDate.now();
 
+        // String zur speicherung der gedrehten Array-Daten
         String gedrehteBilddatenString = this.format + "\n" + this.hoehe + " " +this.breite + "\n" + this.maxHelligkeit + "\n";
+
         for (int i = 0; i < this.breite; i++) {
             for (int j = 0; j < this.hoehe; j++) {
+                // prüft die Stellenanzahl der Zahlenwerte und setzt dementsprechend viele Leerzeichen
                 if (this.gedrehteBilddaten[i][j] > 10) {
                     gedrehteBilddatenString = gedrehteBilddatenString.concat(" ");
                 }
                 else if (j != 0) {
                     gedrehteBilddatenString = gedrehteBilddatenString.concat("  ");
                 }
+                // fügt die Werte einer Zeile zum String hinzu
                 gedrehteBilddatenString = gedrehteBilddatenString.concat(String.valueOf(this.gedrehteBilddaten[i][j]));
             }
+            // fügt einen Zeilenumbruch an das Ende einer Zeile
             gedrehteBilddatenString = gedrehteBilddatenString.concat("\n");
         }
 
-        Path path = Paths.get(verzeichnis, date+".pgm");
+        // erstellt und speichert die neue Datei zwischen
+        Path pfadGedrehtesBild = Paths.get(verzeichnis, date+".pgm");
 
+        // versucht die neue Datei im übergebenen Verzeichnis mit gedrehteBilddatenString zu schreiben
         try {
-            Files.writeString(path, gedrehteBilddatenString);
+            Files.writeString(pfadGedrehtesBild, gedrehteBilddatenString);
         } catch (IOException e) {
-            System.out.println("Gedrehtes Bild konnte nicht in dem gegebenen Verzeichnis gespeichert werden!");
+            System.out.println("Gedrehtes Bild konnte nicht in dem übergebenen Verzeichnis gespeichert werden!");
             e.printStackTrace();
         }
+    }
 
+
+    // Methode gibt die gelesenen und verarbeiteten Daten auf der Konsole aus
+    public void printBilddaten() {
+
+        System.out.println("Format: " + this.format);
+        System.out.println("Breite: " + this.breite);
+        System.out.println("Höhe: " + this.hoehe);
+        System.out.println("Maximalwert für die Helligkeit: " + this.maxHelligkeit);
+
+        System.out.println("Bilddaten in einem 2D Array: ");
+        for (int i = 0; i < this.bilddaten.length; i++) {
+            for (int j = 0; j < this.bilddaten[i].length; j++) {
+                if (this.bilddaten[i][j] > 10) {
+                    System.out.print(" ");
+                }
+                else if (j != 0) {
+                    System.out.print("  ");
+                }
+                System.out.print(this.bilddaten[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+
+    // Methode gibt die Daten des gedrehten Bildes auf der Konsole aus
+    public void printGedrehteBilddaten() {
+
+        System.out.println("Format: " + this.format);
+        System.out.println("Breite: " + this.hoehe);
+        System.out.println("Höhe: " + this.breite);
+        System.out.println("Maximalwert für die Helligkeit: " + this.maxHelligkeit);
+
+        System.out.println("Gedrehte Bilddaten in einem 2D Array: ");
+        for (int i = 0; i < this.gedrehteBilddaten.length; i++) {
+            for (int j = 0; j < this.gedrehteBilddaten[i].length; j++) {
+                if (this.gedrehteBilddaten[i][j] > 10) {
+                    System.out.print(" ");
+                }
+                else if (j != 0) {
+                    System.out.print("  ");
+                }
+                System.out.print(this.gedrehteBilddaten[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
--- src/BildTest.java
+++ src/BildTest.java
@@ -1,9 +1,20 @@
 public class BildTest {
     public static void main(String[] args) {
 
+        // Test von Bild.java
         Bild bild = new Bild();
+
+        // einlesen und verarbeiten des Bildes
         bild.leseBild("java.pgm");
+        System.out.println("Ursprüngliches Bild:");
+        bild.printBilddaten();
+
+        System.out.println();
+
+        // rotieren des Bildes
         bild.rotateBild();
+        System.out.println("Gedrehtes Bild:");
+        bild.printGedrehteBilddaten();
         bild.schreibeBild("src/");
 
     }
